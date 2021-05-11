d37985594e3a33e459f3e3e7488397596b0c4985
bripapp <brianna.papparotto@gmail.com>
2021-05-11 12:08:39 -0400

    svg setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-// @TODO: YOUR CODE HERE!
+// SVG SETUP //
+// svg height and width
+let svgWidth = 1000;
+let svgHeight = 650;
+
+// svg borders
+let margin = {
+    top: 20,
+    bottom: 200,
+    left: 100,
+    right: 40,
+}
