c3c32f81beb10a688d8cd63b0d1961b4edfeab8f
bripapp <brianna.papparotto@gmail.com>
2021-05-11 12:19:15 -0400

    complete svg setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,23 @@ let margin = {
     bottom: 200,
     left: 100,
     right: 40,
-}
+};
+
+// calculate chart height and width
+let width = svgWidth - margin.right - margin.left;
+let height = svgHeight - margin.top - margin.bottom;
+
+// append div class to scatter element
+let chart = d3.select('#scatter')
+    .append('div') 
+    .classed('chart', true);
+
+// append svg element to chart
+let svg = chart.append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
+// append svg group
+let chartGroup = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
